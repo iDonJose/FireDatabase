3be025d2154655501e8fc9190e396c6275ffea85
iDonJose <donor.develop@gmail.com>
2019-02-03 16:58:24 +0100

    Initial commit

---
+++ Sources/Header.h
@@ -0,0 +1,19 @@
+//
+//  <Framework>.h
+//  <Framework>
+//
+//  Created by José Donor on 10/11/2018.
+//  Copyright © 2018 iDonJose. All rights reserved.
+//
+
+//#import <UIKit/UIKit.h>
+
+//! Project version number for <Framework>.
+FOUNDATION_EXPORT double <Framework>VersionNumber;
+
+//! Project version string for <Framework>.
+FOUNDATION_EXPORT const unsigned char <Framework>VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <<Framework>/PublicHeader.h>
+
+
